1f62300e51cba73ba5a6b3e643e8beb99004604b
Grégoire Virepinte <gregoire.virepinte@carbometrix.com>
2021-10-26 17:46:44 +0200

    modified ducusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,14 +6,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'carbometrix calculator',
+  tagline: 'Documentation for the calculator.',
+  url: 'https://gregoire-virepinte.github.io',
+  baseUrl: '/new_documentation/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'gregoire-virepinte', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
   presets: [
